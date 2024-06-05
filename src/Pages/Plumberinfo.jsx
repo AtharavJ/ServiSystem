67e5f68f260522bf3ac967b471de3df3e99181c7
Atharav  Jadhav <atharav3233@gmail.com>
2024-06-05 23:12:15 +0530

    changes in Plumberinfo.jsx

--- src/Pages/Plumberinfo.jsx
+++ src/Pages/Plumberinfo.jsx
@@ -3,7 +3,7 @@
 export default function Plumberinfo() {
 
     const plumbers = [
-        { name: 'Vaibhav Avinash Tiwari', phone: '1234567891', rating: '5', Image: '' },
+        { name: 'Vaibhav Avin Tiwari', phone: '1234567891', rating: '5', Image: '' },
         { name: 'Sima Naresh Maske', phone: '1234567891', rating: '4', Image: '' },
         { name: 'Simran Akbar Malik', phone: '1234567891', rating: '4', Image: '' },
         { name: 'Ramesh Anil Kale', phone: '1234567891', rating: '3', Image: '' },
@@ -15,75 +15,27 @@ export default function Plumberinfo() {
 
     return (
         <>
-
             <h2 className="text-center my-8 px-4 text-3xl font-semibold
                  hover:scale-110">All Plumbers Information</h2>
 
             <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 w-full h-full max-w-screen-lg mx-auto ">
+
                 {plumbers.map((plumbers, index) => (
-                    <div key={index} className="w-full h-72 m-4 border-solid border-4 rounded-md flex flex-col space-y-2 items-center justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" /*style={{ backgroundImage=`url(${})`}}*/>
+                    <div
+                        key={index} className=" relative w-full h-72 m-4 border-solid border-4 rounded-md flex flex-col space-y-2 items-center justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" /*style={{ backgroundImage=`url(${})`}}*/>
+                        <div className="absolute bottom-0 left-0 right-0  bg-opacity-50 p-2 rounded-b-md">
+                            <h2 className="text-xl font-bold bg-opacity-50 px-2 py-1 rounded">
+                                {plumbers.name}</h2>
 
-                        <h2 className="text-xl font-bold bg-opacity-50 px-2 py-1 rounded">
-                            {plumbers.name}</h2>
+                            <h4 className="text-lg bg-opacity-50 px-2 py-1 rounded">
+                                Mobile No: {plumbers.phone}</h4>
 
-                        <h4 className="text-lg bg-opacity-50 px-2 py-1 rounded">
-                            Mobile No: {plumbers.phone}</h4>
+                            <p className="text-md bg-opacity-50 px-2 py-1 rounded">
+                                Rating: {plumbers.rating}</p>
+                        </div>
 
-                        <p className="text-md bg-opacity-50 px-2 py-1 rounded">
-                            Rating: {plumbers.rating}</p>
                     </div>
-                ))
-                }
-
-                {/* <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Vaibhav Avinash Tiwari</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className="w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl"  >
-                    <h2>Sima Naresh Maske</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Simran Akbar Malik</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Ramesh Anil Kale</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Johany Akbar Sins</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Narendra Vilas Khot</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Ajay Ram Patil</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-                </div>
-
-                <div className=" w-full h-72  m-4 border-solid border-4	rounded-md flex space-x-6 items-end justify-center bg-cover bg-center transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-2xl" >
-                    <h2>Amit Arvind Mote</h2>
-                    <h4>Mobile No:1234567891</h4>
-                    <p>Rating:</p>
-
-                </div> */}
-
+                ))}
             </section>
         </>
     )
